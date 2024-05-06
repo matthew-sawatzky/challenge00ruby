cce3871ca018e7a04e0d45512fc547614bb101e3
MatthewSawatz <msawatzky2@rrc.ca>
2024-05-06 12:44:15 -0500

    third commit

--- challenge00.rb
+++ challenge00.rb
@@ -1,5 +1,8 @@
 # Matthew Sawatzky
 
+puts "Enter the subtotal: "
+sub_total_input = gets.chomp
+
 sub_total = 5.00
 
 GST = 0.07
@@ -11,3 +14,11 @@ puts "Subtotal: $#{sub_total}"
 puts "PST: $#{sub_total * PST} - 7%"
 puts "GST: $#{sub_total * GST} - 5%"
 puts "Grand Total: $#{grand_total}"
+
+if grand_total <= 5.00
+    puts 'Pocket Change'
+elsif (grand_total > 5.00 && grand_total < 20.00)
+    puts 'Wallet Time'
+else
+    puts 'Charge It!'
+end
